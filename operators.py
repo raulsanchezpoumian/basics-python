3fa3264c1f94b536e2b4c87d3413000b66a1b1f8
raulsanchezpoumian@gmail.com <raulsanchezpoumian@gmail.com>
2023-04-24 20:31:19 -0600

    ADD: Opperators

---
+++ operators.py
@@ -0,0 +1,22 @@
+
+addition = 10 + 5 # Esto dará como resultado 15
+substraction = 10 - 5 # Esto dará como resultado 5
+multiplication = 10 * 5 # Esto dará como resultado 50
+
+### Comparision operators
+age = 10 == 5 # Esto dará como resultado False
+age = 10 != 5 # Esto dará como resultado True
+age = 10 > 5 # Esto dará como resultado True
+
+### Logical Operators
+age_boy = 10
+age_girl = 14
+
+age_boy > 8 and age_girl > 8 # True
+
+### Assignment Operators
+age = 10 # Dara 10
+
+age += 5 # Dara 15
+age -= 5 # Dara 10
+age *= 5 # Dara 50
